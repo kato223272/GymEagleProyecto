8d8e1a72913005ace28e383c2739cae3d45522b2
kato223272 <223272@ids.upchiapas.edu.mx>
2023-10-22 01:54:29 -0600

    Lista de asistencia pre-terminada

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Ganancias from './Pages/Ganancias';
 
 function App() {
   const currentPath = window.location.pathname;
-  const navbarDisplay = currentPath !== '/' && currentPath !== '/Menu' && currentPath !== '/RecuperarContraseña' ? "block" : "none";
+  const navbarDisplay = (currentPath !== '/' && currentPath !== '/Menu' && currentPath !== '/RecuperarContraseña') ? "block" : "none";
 
   return (
     <Router>
--- src/Pages/EditarUsuario.jsx
+++ src/Pages/EditarUsuario.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const EditarUsuario = () => {
   return (
+    <>
     <div>
       
     </div>
+    </>
   )
 }
 
