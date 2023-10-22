025a5fc966ef68520e1a6c39e79b353e59f96547
paolis28 <shinge@outlook.es>
2023-10-21 19:34:04 -0600

    SE ARREGLO LOS CONTENEDORES DE LOS ICONOS Y LOS ICONOS SE SUSTITUYERON POR MEDIDAS IGUALES, VISTA MENU TERMINADA

--- src/Pages/RecuperarContrasenia.jsx
+++ src/Pages/RecuperarContrasenia.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import {Button, Image, Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+import {Button, Image, Form, FormGroup } from 'react-bootstrap';
 import '../Css/RecuperarContrasenia.css';
 import imgGrande from '../Image/LogoGym.png';
+import emailjs from '@emailjs/browser';
 
 const RecuperarContrasenia = () => {
+  const [User, setUser]=useState("");
+  const [Correo, setCorreo]=useState("");
+
+  const handleUserChange =(e)=>{
+    setUser(e.target.value);
+  }
+  const handleCorreoChange=(e)=>{
+    setCorreo(e.target.value);
+  }
+
+  const frmContact={userEmail:"", contenido:""};
+  const [contact,setcontact]=useState(frmContact);
+
+  const handleSubmit = async()=>{
+    const usuario = User;
+    const correo = Correo;
+    
+    const contact = {userEmail:correo, contenido:""}
+    setcontact({...contact, correo});
+    emailjs.send("default_service", "template_wewxu8e",contact, "MIRXkfk6HhFixndWM")
+    .then((response) => {
+      console.log("SUCCESS", response.status, response.text);
+      setcontact(frmContact);
+    }, (err) => {
+      console.log("ERROR", err); 
+      }
+    )
+  };
+  
+
   return (
     <div className='containerGlobal'>
       <header>
         <nav className='NavbarSesion'>
-          {/* <a href='/'><Button variant='dark'>Regresar</Button></a> */}
           <h1>Recuperar contraseña</h1>
         </nav>
       </header>
@@ -19,19 +49,33 @@ const RecuperarContrasenia = () => {
             <Image className='imgGrande' src={imgGrande}></Image>
             <h1 className='tituloColum1'>Black Eagle Gym</h1>
           </div>
-
           <div className='columna2'>
-            <label className='indicacionRecuperar'>Ingrese usuario:</label>
-            <Form.Control className='inputsRecuperar' type="text" placeholder="Usuario" />
 
-            <label className='indicacionRecuperar'>Colocar número télefonico:</label>
-            <Form.Control className='inputsRecuperar' type="text" placeholder="Ejem: 9605658789" />
-            <Button className='bt-Recuperar' variant='warning'>Eviar mensaje</Button>
+            <FormGroup>
+              <label className='indicacionRecuperar'>Ingrese usuario:</label>
+              <Form.Control 
+              className='inputsRecuperar' 
+              type="text" 
+              placeholder="Usuario"
+              value={User}
+              onChange={handleUserChange}/>
+            </FormGroup>
+
+            <FormGroup>
+              <label className='indicacionRecuperar'>Correo electronico:</label>
+              <Form.Control 
+              className='inputsRecuperar' 
+              type="text" 
+              placeholder="Ejem: 9605658789" 
+              value={Correo}
+              onChange={handleCorreoChange}/>
+            </FormGroup>
+
+            <Button className='bt-Recuperar' variant='warning' onClick={handleSubmit}>Eviar 
+            mensaje</Button>
 
             <label className='indicacionRecuperar'>Ingrese contraseña nueva:</label>
-            <Form.Control className='inputsRecuperar' type="password" placeholder="Contraseña Nueva" 
-            disabled/>
-
+            <Form.Control className='inputsRecuperar' type="password" placeholder="Contraseña Nueva"/>
             <Button className='bt-Recuperar' variant='warning'>Acceder</Button>
           </div>
         </div>
