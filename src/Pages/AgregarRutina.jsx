69e782e69f615a23fd81e1f167ee05a65a614f04
paolis28 <shinge@outlook.es>
2023-11-11 13:51:39 -0600

    Hice cambios en las vistas de rutinar y usuarios, se agregaron tablas y modales, en la vista de ganancia ya se agregaron, se probaron los parametros en el modal para pasar datos de la tabla de asistencia al modal

--- src/Pages/AgregarRutina.jsx
+++ src/Pages/AgregarRutina.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../Css/AgregarRutina.css';
+import {Col, Row, InputGroup, Form} from  'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function AgregarRutina (){
   return(
@@ -11,44 +14,52 @@ function AgregarRutina (){
         <label className='labRutina'>Nombre:</label>
         <input type="text" placeholder="Nombre de rutina" className='inputRutina'/>
         
-        <label className='labRutina'>Descripción de la rutina</label>
+        <label className='labRutina'>Descripción de la rutina:</label>
         <textarea rows={2} placeholder="Descripción" className='inputRutinaDescrip'
         style={{paddingTop:'1%'}}></textarea>
 
-        <label className='labRutina'>Tiempo promedio de resultados</label>
-        <select className='inputRutina'>
-        <option disabled='false'>Resultados visto en:</option>
-            <option value='1'>1-15 Dias</option>
-            <option value='2'>15-30 Días</option>
-            <option value='3'>1-2 Meses</option>
-            <option value='4'>1-3 Meses</option>
-        </select>
+      <Row>
+        <Col>
+          <label className='labRutina'>Series por día:</label>
+          <select className='inputRutinaSelec'>
+            <option disabled='false'>Resultados visto en:</option>
+              <option value='1'>3</option>
+              <option value='2'>4</option>
+              <option value='3'>5</option>
+          </select>
+        </Col>
 
-
-
-        <label className='labRutina'>Dato adicional</label>
-        <input type="text" placeholder="Otros" className='inputRutina'/>
+        <Col>
+          <label className='labRutina'>Repeticiones:</label>
+          <select className='inputRutinaSelec'>
+            <option disabled='false'>Repeticiones por día:</option>
+              <option value='1'>6</option>
+              <option value='2'>8</option>
+              <option value='3'>10</option>
+              <option value='4'>12</option>
+          </select>
+        </Col>
+      </Row>
+        <button className='btRutina'>Guardar Registro</button>
       </div>
 
       <div className='Ruticolumna2'>
-        <h3 className='tituRutina'>Beneficios Corporales</h3>
-          <label className='labRutina'>Edad:</label>
-          <input type="text" placeholder="Nombre de rutina" className='inputRutina'/>
-
-          <label className='labRutina'>Beneficios</label>
-          <textarea rows={5} placeholder="Descripción" className='inputRutinaDescrip'
-          style={{paddingTop:'1%'}}></textarea>
-
-          <label className='labRutina'>Estatura</label>
-          <input type="text" placeholder="Nombre de rutina" className='inputRutina'/>
-
-          <label className='labRutina'>Peso</label>
-          <input type="text" placeholder="Nombre de rutina" className='inputRutina'/>
+        <h3 className='tituRutina'>Rutinas Registradas: </h3>
+        <InputGroup>
+          <InputGroup.Text id="basic-addon1">
+            <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: '#000000' }} />
+          </InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Buscar"
+              className=" mr-sm-2"
+            />
+        </InputGroup>
+        <select className='ListaRutinasSelect'>
+            <option value='1'>Rutina</option>
+        </select>
       </div>
       
-      </div>
-      <div className='containerButton'>
-      <button className='btRutina'>Guardar Registro</button>
       </div>
     </>
   )
--- src/Pages/EditarUsuario.jsx
+++ src/Pages/EditarUsuario.jsx
@@ -1,130 +1,45 @@
 import React, { useState } from 'react';
 import "../Css/EditarUsuario.css";
-import { FaSearch } from "react-icons/fa";
-import Form from 'react-bootstrap/Form';
-import imgEliminar from "../Image/imgEditar/eliminarOp2.png";
-import imgEliminarHover from "../Image/imgEditar/eliminarOp2Hover.png";
-import imgAgregar from "../Image/imgEditar/agregarOp1.png";
-import imgAgregarHover from "../Image/imgEditar/agregarOp1Hover.png";
+import {Form, Row, Col} from 'react-bootstrap';
 
 const EditarUsuario = () => {
-  const fechaActual = new Date();
-  const fechaFormateada = fechaActual.toLocaleDateString();
-
-  const [imagenHoverEliminar, setImagenHoverEliminar] = useState(imgEliminarHover);
-  const [imagenHoverAgregar, setImagenHoverAgregar] = useState(imgAgregarHover);
- 
-
-  const handleImagenHoverEliminar = (nuevaImagen) => {
-    setImagenHoverEliminar(nuevaImagen);
-  };
-  const handleImagenHoverAgregar = (nuevaImagen) => {
-    setImagenHoverAgregar(nuevaImagen);
-  };
-
   return (
     <>
-      <div className="FilaSuperior" style={{ display: "flex" }}>
-        <div className="fecha">
-          <p>Fecha: {fechaFormateada}</p>
-        </div>
-        <div className="busqueda-contenedor">
-          <input type="text" placeholder="Buscar" className="busqueda" />
-          <button className="busqueda-boton">
-            <FaSearch />
-          </button>
-        </div>
-      </div>
+      <div className='contenerdorEditar'>
+        <div className='Editarcolumna1'>
+          <h3 className='tituRutina'>Registro de Usuario</h3>
 
-      <div className="contenerdorEditar" style={{ display: "flex" }}>
-        <div className="col1">
-        
-          <div className="EditarFila1">
-            <div className="BotonEliminar"
-            
-            >
-              <button
-              onMouseEnter={() => handleImagenHoverEliminar(imgEliminarHover)}
-              onMouseLeave={() => handleImagenHoverEliminar(imgEliminar)}
-              >
-              <img
-            src={imagenHoverEliminar}
-            alt=""          
-            />
-              </button>
-            </div>
-            <h4>Editar Datos</h4>
-            <div className="BotonAgregar"
+          <label className='labEditar'>Nombre:</label>
+          <input type="text" placeholder="Nombre de usuario" className='inputEditar'/>
 
-            > 
-              <button            
-              onMouseEnter={() => handleImagenHoverAgregar(imgAgregarHover)}
-              onMouseLeave={() => handleImagenHoverAgregar(imgAgregar)}>
-              <img
-            src={imagenHoverAgregar}
-            alt=""          
-            />
-              </button>
-            </div>
-          </div>
-          
-          <div className="EditarFila2">
-            <div className="row datos">
-              <label for="nombre">Nombre:</label>
-              <Form.Control
-                type="text"
-                id="nombre"
-                placeholder="Escribe nombre"
-                className="escribirDatos"
-              />
-            </div>
-            <div className="row datos">
-              <label for="ApellidoPaterno">Apellido Paterno:</label>
-              <Form.Control
-                type="text"
-                placeholder="Escribe apellido paterno"
-                className="escribirDatos"
-              />
-            </div>
-            <div className="row datos">
-              <label for="nombre">Apellido Materno:</label>
-              <Form.Control
-                type="text"
-                placeholder="Escribe apellido materno"
-                className="escribirDatos"
-              />
-            </div>
-            <div className="row datos">
-              <label for="nombre">Número telefonico:</label>
-              <Form.Control
-                type="text"
-                placeholder="Escribe num. telefonico"
-                className="escribirDatos"
-              />
-            </div>
-            <div className="BotonGuardar">
-              <button>Guardar</button>
-            </div>
-          </div>
-        </div>
+          <label className='labEditar'>Apellido Paterno:</label>
+          <input type="text" placeholder="Primer Apellido" className='inputEditar'/>
 
-        <div className="col2">
-          <h4 >Renovación de pagos</h4>
-          <div className="NombreCol2">
-            <label for="nombre">Nombre:</label>
-            <Form.Control type="text" value="NombreText" disabled />
-          </div>
-          <div className="RenovarPago" style={{marginTop:'2%'}}>
-          <label for="nombre">Renovar pago:</label>
-          <button> Renovar</button>
-          </div>
-          <div className="RenovarPago" style={{marginTop:'2%'}}>
-          <label for="nombre">Renovar pago 1 día:</label>
-          <button> Renovar</button>
-          </div>
-          <div className="BotonGuardar2"  style={{marginTop:'5%'}}>
-              <button>Guardar</button>
-            </div>
+          <label className='labEditar'>Apellido Materno:</label>
+          <input type="text" placeholder="Segundo Apellido" className='inputEditar'/>
+
+          <label className='labEditar'>Teléfono:</label>
+          <input type="text" placeholder="Ejem.96012030505" className='inputEditar'/>
+
+          <Form.Check
+            style={{color:'white', marginTop:'3%', marginLeft:'3%'}}
+            inline
+            label="Plan mensual"
+            aria-label="option 1"/>
+
+          <Form.Check
+            style={{color:'white', marginTop:'3%'}}
+            inline
+            label="Solo un día"
+            aria-label="option 1"/><br/>
+
+          <Form.Check
+            style={{color:'white', marginTop:'3%', marginLeft:'3%'}}
+            inline
+            label="Adquirio rutina"
+            aria-label="option 1"/><br/>
+
+          <button className='btEditar'>Guardar Registro</button>
         </div>
       </div>
     </>
