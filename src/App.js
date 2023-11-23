c57628f2fc99102dcc576d02d2bc0db553d5a2cb
kato223272 <223272@ids.upchiapas.edu.mx>
2023-11-23 00:50:09 -0600

    cambios previos rutas protejidas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-import { AuthProvider } from './AuthContext';
-import ProtectedRoute from './RutaProtejida.js';
+import { useState } from 'react';
 
 import Navbar from './Components/Navbar';
 import Inicio from '../src/Inicio';
@@ -13,6 +12,7 @@ import Recuperar from './Pages/RecuperarContrasenia';
 import Ganancias from './Pages/Ganancias';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const currentPath = window.location.pathname.toLowerCase(); // Convierte a minusculas 
   const navbarDisplay = (currentPath !== '/' && currentPath !== '/menu' && currentPath !== '/recuperarcontrasenia') ? "block" : "none";
 
@@ -22,13 +22,13 @@ function App() {
         <Navbar />
       </div>
       <Routes>
-      <Route path="/" element={<Inicio />} />
-        <ProtectedRoute path="/menu" element={<Menu />} />
-        <ProtectedRoute path="/rutinas" element={<Rutina />} />
-        <ProtectedRoute path="/asistencias" element={<Asistencias />} />
-        <ProtectedRoute path="/editar" element={<Editar />} />
+      <Route path="/" element={<Inicio setIsAuthenticated={setIsAuthenticated} isAuthenticated={isAuthenticated} />} />
+        <Route path="/menu" element={isAuthenticated ? <Menu /> : <Navigate to="/" />} />
+        <Route path="/rutinas" element={isAuthenticated ? <Rutina /> : <Navigate to="/" />} />
+        <Route path="/asistencias" element={isAuthenticated ? <Asistencias /> : <Navigate to="/" />} />
+        <Route path="/editar" element={isAuthenticated ? <Editar /> : <Navigate to="/" />} />
         <Route path="/recuperarcontrasenia" element={<Recuperar />} />
-        <ProtectedRoute path="/ganancias" element={<Ganancias />} />
+        <Route path="/ganancias" element={isAuthenticated ? <Ganancias /> : <Navigate to="/" />} />
       </Routes>
     
     </Router>
