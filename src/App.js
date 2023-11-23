9783c5334b2deefc7a55798001b1fe965eb0a30d
kato223272 <223272@ids.upchiapas.edu.mx>
2023-11-23 01:10:02 -0600

    Rutas protegidas en archivo aparte

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import './App.css';
 import { useState } from 'react';
-
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Inicio from '../src/Inicio';
 import Rutina from './Pages/AgregarRutina';
@@ -10,30 +9,48 @@ import Asistencias from './Pages/ListaAsistencias';
 import Editar from './Pages/EditarUsuario.jsx';
 import Recuperar from './Pages/RecuperarContrasenia';
 import Ganancias from './Pages/Ganancias';
+import ProtectedRoute from './PrivateRoute.js';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const currentPath = window.location.pathname.toLowerCase(); // Convierte a minusculas 
-  const navbarDisplay = (currentPath !== '/' && currentPath !== '/menu' && currentPath !== '/recuperarcontrasenia') ? "block" : "none";
 
   return (
     <Router>
-      <div style={{ display: navbarDisplay }}>
-        <Navbar />
+      <div style={{ display: isAuthenticated ? 'block' : 'none' }}>
+        {isAuthenticated && <Navbar />}
       </div>
       <Routes>
-      <Route path="/" element={<Inicio setIsAuthenticated={setIsAuthenticated} isAuthenticated={isAuthenticated} />} />
-        <Route path="/menu" element={isAuthenticated ? <Menu /> : <Navigate to="/" />} />
-        <Route path="/rutinas" element={isAuthenticated ? <Rutina /> : <Navigate to="/" />} />
-        <Route path="/asistencias" element={isAuthenticated ? <Asistencias /> : <Navigate to="/" />} />
-        <Route path="/editar" element={isAuthenticated ? <Editar /> : <Navigate to="/" />} />
-        <Route path="/recuperarcontrasenia" element={<Recuperar />} />
-        <Route path="/ganancias" element={isAuthenticated ? <Ganancias /> : <Navigate to="/" />} />
+        <Route
+          path="/"
+          element={<Inicio setIsAuthenticated={setIsAuthenticated} isAuthenticated={isAuthenticated} />}
+        />
+        <Route
+          path="/menu"
+          element={<ProtectedRoute element={<Menu />} isAuthenticated={isAuthenticated} />}
+        />
+        <Route
+          path="/rutinas"
+          element={<ProtectedRoute element={<Rutina />} isAuthenticated={isAuthenticated} />}
+        />
+        <Route
+          path="/asistencias"
+          element={<ProtectedRoute element={<Asistencias />} isAuthenticated={isAuthenticated} />}
+        />
+        <Route
+          path="/editar"
+          element={<ProtectedRoute element={<Editar />} isAuthenticated={isAuthenticated} />}
+        />
+        <Route
+          path="/recuperarcontrasenia"
+          element={<Recuperar />}
+        />
+        <Route
+          path="/ganancias"
+          element={<ProtectedRoute element={<Ganancias />} isAuthenticated={isAuthenticated} />}
+        />
       </Routes>
-    
     </Router>
   );
 }
-
 
 export default App;
