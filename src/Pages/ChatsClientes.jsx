9c1f007e0c33f70afe3b6316867449fa3d237993
paolis28 <shinge@outlook.es>
2023-12-03 03:01:16 -0600

    Implementación del chat en tiempo real, conexión de administrador y usuarios

--- src/Pages/ChatsClientes.jsx
+++ src/Pages/ChatsClientes.jsx
@@ -1,5 +1,9 @@
 import React,{useState,useEffect} from "react";
+import io from 'socket.io-client'
+import axios from 'axios'
 import '../Css/ChatsClientes.css'
+
+const socket = io('http://localhost:4000')
 
 const ChatsClientes = () => {
     const [nickname, setNickname] = useState('')
@@ -80,11 +84,69 @@ const ChatsClientes = () => {
 
     return(
         <div className="containerChats">
-            <div className="containerClientes">
+            {/* <div className="containerClientes">
                 <p>Div lateral para los clientres</p>
-            </div>
+            </div> */}
 
             <div className="containerMensajes">
+            <div className="container mt-3">
+
+<div className="card shadow border-0">
+<div className="card-body">
+  <h5 className="text-center mb-3">CHAT</h5>
+
+  {/* nickname */}
+
+  <form onSubmit={nicknameSubmit}>
+    <div className="d-flex mb-3">
+      <input type="text" className="form-control" id="nickname" placeholder="Nickname..." disabled={disabled} onChange={e => setNickname(e.target.value)} value={nickname} required/>
+      <button className="btn btn-success mx-3" type="submit" id="btn-nickname" disabled={disabled}>Establecer</button>
+    </div>
+  </form>
+
+  {/* chat form */}
+
+  <form onSubmit={handlerSubmit}>
+    <div className="d-flex">
+      <input type="text" className="form-control" placeholder="Mensaje..." onChange={e => setMessage(e.target.value)} value={message}/>
+      <button className="btn btn-success mx-3" type="submit">Enviar</button>
+    </div>
+  </form> 
+</div>
+</div>
+
+{/* chat messages */}
+
+<div className="card mt-3 mb-3 shadow border-0" id="content-chat">
+<div className="card-body">
+
+  {messages.map((message, index) => (
+    <div key={index} className={`d-flex p-3 ${message.from === "Yo" ? "justify-content-end" : "justify-content-start"}`}>
+      <div className={`card mb-3 shadow border-1 ${message.from === "Yo" ? "bg-success bg-opacity-25" : "bg-light"}`}>
+        <div className="card-body">
+          <small className="">{message.from}: {message.body}</small>
+        </div>
+      </div>
+    </div>
+
+  ))}
+
+  {/* chat stored messages */}
+  <small className="text-center text-muted">... Mensajes guardados ...</small>
+  {storedMessages.map((message, index) => (
+    <div key={index} className={`d-flex p-3 ${message.from === nickname ? "justify-content-end" : "justify-content-start"}`}>
+      <div className={`card mb-3 shadow border-1 ${message.from === nickname ? "bg-success bg-opacity-25" : "bg-light"}`}>
+        <div className="card-body">
+          <small className="text-muted">{message.from}: {message.message}</small>
+        </div>
+      </div>
+    </div>
+
+  ))}
+
+</div>
+</div>
+</div>
             </div>
         </div>
     );
